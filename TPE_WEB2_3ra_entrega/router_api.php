c6f3e1ab47bddd5d23a543525c9b5ddc37e3db87
Mateofoschino <90288125+Mateofoschino@users.noreply.github.com>
2023-11-13 13:50:18 -0300

    actualizo router con nuevos endpoints

--- TPE_WEB2_3ra_entrega/router_api.php
+++ TPE_WEB2_3ra_entrega/router_api.php
@@ -10,7 +10,10 @@ $router = new Router();
 $router->addRoute('goleadores', 'GET', 'ApiController', 'getAll');
 $router->addRoute('goleadores/:ID', 'GET', 'ApiController', 'getAll');
 $router->addRoute('goleadores/:ID', 'DELETE', 'ApiController', 'delete');
+$router->addRoute('goleadores', 'DELETE', 'ApiController', 'delete');
 $router->addRoute('goleadores', 'POST', 'ApiController', 'add');
+$router->addRoute('goleadores/:ID', 'PUT', 'ApiController', 'modify');
+$router->addRoute('goleadores', 'PUT', 'ApiController', 'modify');
 // rutea
 $router->route($_REQUEST['resource'], $_SERVER['REQUEST_METHOD']);
 
