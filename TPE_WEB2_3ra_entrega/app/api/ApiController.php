d75e1365cef2fce2c44c54882f24ca9f958bb7c0
Mateofoschino <90288125+Mateofoschino@users.noreply.github.com>
2023-12-05 02:28:12 -0300

    Actualizo controller
    Agrego modify, metodo al cual redirigia PUT, que no estaba.

--- TPE_WEB2_3ra_entrega/app/api/ApiController.php
+++ TPE_WEB2_3ra_entrega/app/api/ApiController.php
@@ -3,12 +3,13 @@ require_once 'app/api/abstractApiController.php';
 require_once 'app/models/goleadores.model.php';
 require_once 'app/api/api.view.php';
 
-class ApiController extends abstractApiController {
-    
-    public function __construct() {
+class ApiController extends abstractApiController
+{
+
+    public function __construct()
+    {
         parent::__construct();
         $this->model = new goleadoresModel();
-        
     }
 
     // $params al poder ser null puede ser invocado para listar todo o
@@ -17,19 +18,17 @@ class ApiController extends abstractApiController {
     public function getAll($params = null)
     {
         $parametros = [];
-        if (isset($_GET['order'])) {
-            $parametros['order']=$_GET['order'];
-        }
-        if (isset($_GET['sort'])) {
-            $parametros['sort']=$_GET['sort'];
-        }
+        if (isset($_GET['order']) && isset($_GET['sort'])) {
+            $parametros['order'] = $_GET['order'];
+            $parametros['sort'] = $_GET['sort'];
+        } 
 
         if (!isset($params[':ID'])) {
             $goleadores = $this->model->getGoleadores($parametros);
             $this->view->response($goleadores, 200);
         } else {
             $idGoleador = $params[':ID'];
-            $goleador = $this->model->getDetailsById($idGoleador);
+            $goleador = $this->model->getGoleadorById($idGoleador);
             if ($goleador) {
                 $this->view->response($goleador, 200);
             } else {
@@ -38,18 +37,23 @@ class ApiController extends abstractApiController {
         }
     }
 
-    public function delete($params=null) {
-        $idGoleador=$params[':ID'];
-        $success = $this->model->deleteGoleador($params[':ID']);
-        if (!$success) {
-            $this->view->response("El jugador con el id =$idGoleador no existe",404);
+    public function delete($params = null)
+    {
+        if (empty($params)) {
+            $this->view->response("Inserte un ID", 400);
         } else {
-            $this->view->response("El jugador con el id=$idGoleador se borro exitosamente",200);
+            $idGoleador = $params[':ID'];
+            $success = $this->model->deleteGoleador($params[':ID']);
+            if (!$success) {
+                $this->view->response("El jugador con el id =$idGoleador no existe", 404);
+            } else {
+                $this->view->response("El jugador con el id=$idGoleador se borro exitosamente", 200);
+            }
         }
-        
     }
 
-    public function add(){
+    public function add()
+    {
         $body = $this->getData();
 
         $nombre = $body->Nombre;
@@ -57,14 +61,37 @@ class ApiController extends abstractApiController {
         $goles = $body->Goles;
         $pj = $body->PJ;
 
-        $id = $this->model->insertGoleador($nombre,$club,$goles,$pj);
+        $id = $this->model->insertGoleador($nombre, $club, $goles, $pj);
 
         if ($id) {
-            $this->view->response("El goleador fue creado con exito",200);
+            $this->view->response("El goleador fue creado con exito", 201);
         } else {
-            $this->view->response("El goleador no fue creado",404);
+            $this->view->response("El goleador no fue creado", 404);
         }
     }
 
-
+    public function modify($params = null)
+    {
+        if (empty($params)) {
+            $this->view->response("Inserte un ID para modificar", 400);
+        } else {
+            $idGoleador = $params[':ID'];
+            $jugador = $this->model->getGoleadores($idGoleador);
+            if ($jugador) {
+                $body = $this->getData();
+                $nombre = $body->Nombre;
+                $club = $body->Club;
+                $goles = $body->Goles;
+                $pj = $body->PJ;
+                $success = $this->model->modifyGoleador($nombre, $club, $goles, $pj, $idGoleador);
+                if ($success) {
+                    $this->view->response("El jugador fue modificado con exito", 201);
+                } else {
+                    $this->view->response("Bad request", 400);
+                }
+            } else {
+                $this->view->response("El jugador con el id =$idGoleador no existe", 404);
+            }
+        }
+    }
 }
